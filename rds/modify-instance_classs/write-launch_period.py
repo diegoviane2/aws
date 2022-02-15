868d6e02fd0ae9aba7b815ad6eb7a7f98a5cb7a1
Diego Viane <digaodigao1@gmail.com>
2022-02-15 14:11:31 -0300

    Validação simples de data no write-launch_period script

--- rds/modify-instance_classs/write-launch_period.py
+++ rds/modify-instance_classs/write-launch_period.py
@@ -5,14 +5,16 @@
 # wich other scripts can use as parameter                                    #
 # Written by Diego Viane Github: https://github.com/diegoviane2              #
 ##############################################################################
-
+#                   #
 #####################
 # DATETIME FORMAT:  #
 # dd/mm/aaaa HH:MM  #
 #####################
 
+import re
 import csv
 import argparse
+
 
 
 # Handling arguments
@@ -21,6 +23,16 @@ ap.add_argument("-s", "--start", required=True, help="Start Period")
 ap.add_argument("-e", "--end", required=True, help="End Period")
 args = vars(ap.parse_args())
 
+start = args['start']
+end = args['end']
+
+def valid(data):
+    pattern = re.compile(r'\d{2}/\d{2}/\d{4}\s\d{2}:\d{2}')
+    valid = re.match(pattern, data)
+    if valid:
+        return True
+    else:
+        return False
 
 def append_date(start_period, end_period):
     # Adiciona nova linha com as informações recebidas via argumentos em formato CSV
@@ -30,7 +42,14 @@ def append_date(start_period, end_period):
 
 
 #Call Function
-print ("  [ Initializing task - Write file ]")
-print ("  [ Writing ... Start-Period: " + str(args['start']) + " End-Period: " + str(args['end']) + " ... ]")
-append_date(str(args['start']),str(args['end']))
-print ("  [ Write file ... ] [DONE]")
+print ("[ Initializing task - Schedule Launch Period ]")
+if valid(start) and valid(end):    
+    print ("[ Writing ... Start-Period: " + str(args['start']) + " End-Period: " + str(args['end']) + " ... ]")
+    append_date(str(start),str(end))
+    print ("[ Write to file ... ] [ OK ]")
+    print('[ Finalizing task ... ] [ DONE ]')
+    print("[ TASK: Schedule Launch Period  ... ] [ DONE ]")
+else:
+    print("[ Error: One of the values is invalid ... ] [ WARNING ]")
+    print("[ Accepted formats: dd/mm/aaaa HH:MM  ... ] [ WARNING ]")
+    print("[ TASK: Schedule Launch Period  ... ] [ FAILED ]")
